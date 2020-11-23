49a0d01c9f07b8f2132a19132170a07b11a86f61
mike <mikesapienzaaudio@gmail.com>
2020-11-23 16:54:28 -0600

    finished

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -4,7 +4,18 @@ import { movies } from '../data';
 const Movies = () => {
   return (
     <div>
-        {/*{code here}*/}
+        <h1>Movies Page</h1>
+        {movies.map(movie => {
+          return <div>
+            <h1>{movie.title}</h1>
+            <h3>Length: {movie.time}</h3>
+            <ul>
+            {movie.genres.map(genre => {
+              return <li>{genre}</li>
+            })}
+            </ul>
+            </div>
+        })}
     </div>
   );
 };
